2eb4a6ef51a21e6ec46f36c98774db747c15b873
Gtgstg <thevishwajeet007@gmail.com>
2020-12-07 17:17:52 +0530

    config file

---
+++ auth-backend/config.py
@@ -0,0 +1,6 @@
+MAX_SAFE_STRING_LEN	= 512
+AUTH_SERVER_NAME	= "auth.datasetu.org"
+TOKEN_LEN		= 16
+TOKEN_LEN_HEX		= 32
+INTROSPECT_URL		= "https://auth.datasetu.org/auth/v1/token/introspect"
+SERVER_CERTIFICATE	= "certificate.pem.p12"
