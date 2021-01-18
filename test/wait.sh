c9257b46f7b5eb6a6139e0bd9b665619d1a1e3eb
Gtgstg <thevishwajeet007@gmail.com>
2021-01-18 15:18:22 +0530

    action file.

--- test/wait.sh
+++ test/wait.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Wait for the auth server to come up
-until $(curl -k -XPOST --output /dev/null --silent https://video.datasetu.org:3002); do
+until $(curl -k -XPOST --output /dev/null --silent https://localhost:3002); do
 	printf '.'
 	sleep 1
 done
