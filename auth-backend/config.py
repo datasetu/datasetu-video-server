f30841d4b0fd0870d57f7424a78b6f0afce56d6a
Gtgstg <thevishwajeet007@gmail.com>
2021-04-21 20:59:13 +0530

    Minor refactoring

--- auth-backend/config.py
+++ auth-backend/config.py
@@ -2,12 +2,11 @@ import os
 
 MAX_SAFE_STRING_LEN	= 512
 AUTH_SERVER_NAME	= os.environ["AUTH_SERVER"]
-TOKEN_LEN		= 16
 TOKEN_LEN_HEX		= 32
 INTROSPECT_URL		= "https://"+ AUTH_SERVER_NAME + "/auth/v1/token/introspect"
-RS_CERT_FILE	        = "/root/auth-backend/resource-server.pem"
+RS_CERT_FILE	    = "/root/auth-backend/resource-server.pem"
 RS_KEY_FILE	        = "/root/auth-backend/resource-server.key.pem"
-RECORD_SRC_DIR          = '/root/datasetu-video-server/nginx/record/'
-RECORD_TAR_DIR          = '/root/datasetu-video-server/nginx/record'
-HLS_SRC_DIR             = '/root/datasetu-video-server/nginx/storage/rtmp+hls'
-
+RECORD_SRC_DIR      = '/root/datasetu-video-server/nginx/record'
+RECORD_DEST_DIR     = '/root/datasetu-video-server/nginx/record/'
+HLS_SRC_DIR         = '/root/datasetu-video-server/nginx/storage/rtmp+hls'
+HLS_DEST_DIR         = '/root/datasetu-video-server/nginx/storage/rtmp+hls/'
